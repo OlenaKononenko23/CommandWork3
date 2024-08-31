046fb7f8d76f976a7f778ffe0d07151db2cb11e5
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-08-31 19:37:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+ // src/index.js 
+
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+// Middleware для логування часу запиту
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+app.use(express.json());
+
+// Маршрут для обробки GET-запитів на '/'
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, World!',
+  });
+});
+
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
