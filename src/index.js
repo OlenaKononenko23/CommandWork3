f706bdef9cae32f976e989b37c149814fce00dd7
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-08-31 22:22:06 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 // src/index.js
 
-import express from 'express';
+import { startServer } from './server.js';
 
-const PORT = 3000;
-
-const app = express();
-
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
